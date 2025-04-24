41f5a5c740fcb939f4d41e3e8eca8ca46c66c5b5
chaitanyavaddadi-stripe <chaitanyavaddadi@stripe.com>
2025-04-24 21:56:07 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -181,7 +181,7 @@ document.addEventListener('DOMContentLoaded', function() {
      */
     function setupCarousel() {
         const carouselImages = [
-            "images/blog/tree-planting.jpg",
+            "images/blog/childrens-home.jpeg",
             "images/blog/animal-shelter.jpg",
             "images/blog/orphanage.jpg",
             "images/blog/elderly-engagement.jpg"
